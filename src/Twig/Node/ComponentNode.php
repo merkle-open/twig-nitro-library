b9ee7d1ba0f5021831cff5141aae3bb2200f58ef
Robert Vogt <robert.vogt@namics.com>
2016-04-22 12:19:06 +0200

    Resolve merge conflicts

--- src/Twig/Node/ComponentNode.php
+++ src/Twig/Node/ComponentNode.php
@@ -6,51 +6,14 @@ use \Twig_Compiler;
 use \Twig_Node;
 use \Twig_NodeOutputInterface;
 use \Twig_Node_Expression;
-<<<<<<< HEAD
 
 final class ComponentNode extends Twig_Node implements Twig_NodeOutputInterface {
-=======
-
-/**
- * Includes a Terrific Component.
- *
- * <pre>
- *   {% component 'Navigation' %}
- *   {% component 'Navigation' 'primary %}
- *   {% component 'Navigation' with {"active": "home"} %}
- *   {% component 'Navigation' 'primary with {"active": "home"} %}
- * </pre>
- *
- * Class ComponentNode
- * @package Deniaz\Terrific\Twig\Node
- */
-final class ComponentNode extends Twig_Node implements Twig_NodeOutputInterface
-{
-
-  /**
-   * ComponentNode constructor.
-   */
-  public function __construct(Twig_Node_Expression $component, Twig_Node_Expression $data = null, $only = false, $lineno, $tag = null)
-  {
-      parent::__construct(
-      [ 'component' => $component, 'data' => $data ],
-      [ 'only' => (bool) $only ],
-      $lineno,
-      $tag
-    );
-  }
-
-    public function compile(Twig_Compiler $compiler)
-    {
-        $compiler->addDebugInfo($this);
->>>>>>> 78298b060d695c124e89fd7ea92a87e9f6326bd4
 
   /**
    * ComponentNode constructor.
    */
   public function __construct(Twig_Node_Expression $component, Twig_Node_Expression $data = null, $only = false, $lineno, $tag = null) {
 
-<<<<<<< HEAD
     parent::__construct(
       [ 'component' => $component, 'data' => $data ],
       [ 'only' => (bool) $only ],
@@ -71,16 +34,6 @@ final class ComponentNode extends Twig_Node implements Twig_NodeOutputInterface
 
   protected function addGetTemplate(Twig_Compiler $compiler) {
     $compiler
-=======
-        $compiler->raw('->display(');
-        $this->addTemplateArguments($compiler);
-        $compiler->raw(");\n");
-    }
-
-    protected function addGetTemplate(Twig_Compiler $compiler)
-    {
-        $compiler
->>>>>>> 78298b060d695c124e89fd7ea92a87e9f6326bd4
       ->write('$this->loadTemplate(')
       ->subcompile($this->getNode('component'))
       ->raw(', ')
@@ -88,7 +41,6 @@ final class ComponentNode extends Twig_Node implements Twig_NodeOutputInterface
       ->raw(', ')
       ->repr($this->getLine())
       ->raw(')');
-<<<<<<< HEAD
   }
 
   protected function addTemplateArguments(Twig_Compiler $compiler) {
@@ -113,21 +65,3 @@ final class ComponentNode extends Twig_Node implements Twig_NodeOutputInterface
     }
   }
 }
-=======
-    }
-
-    protected function addTemplateArguments(Twig_Compiler $compiler)
-    {
-        if (null === $this->getNode('data')) {
-            $compiler->raw(false === $this->getAttribute('only') ? '$context' : '[]');
-        } elseif (false === $this->getAttribute('only')) {
-            $compiler
-        ->raw('array_merge($context, ')
-        ->subcompile($this->getNode('data'))
-        ->raw(')');
-        } else {
-            $compiler->subcompile($this->getNode('data'));
-        }
-    }
-}
->>>>>>> 78298b060d695c124e89fd7ea92a87e9f6326bd4
--- src/Twig/TokenParser/ComponentTokenParser.php
+++ src/Twig/TokenParser/ComponentTokenParser.php
@@ -12,85 +12,66 @@ use \Twig_Node_Expression_Constant;
  * Class ComponentTokenParser
  * @package Deniaz\Terrific\Twig\TokenParser
  */
-final class ComponentTokenParser extends Twig_TokenParser
-{
-    /**
-     * @var string Twig Template File Extension
-     */
-    private $fileExtension;
+final class ComponentTokenParser extends Twig_TokenParser {
+  /**
+   * @var string Twig Template File Extension
+   */
+  private $fileExtension;
 
-    /**
-     * @param string $fileExtension Twig Template File Extension
-     */
-    public function __construct($fileExtension)
-    {
-        $this->fileExtension = $fileExtension;
+  /**
+   * @param string $fileExtension Twig Template File Extension
+   */
+  public function __construct($fileExtension) {
+    $this->fileExtension = $fileExtension;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function parse(Twig_Token $token) {
+    $component = $this->parser->getExpressionParser()->parseExpression();
+    list($data, $only) = $this->parseArguments();
+
+    return new ComponentNode($component, $data, $only, $token->getLine(), $this->getTag());
+  }
+
+  /**
+   * Parses Tag Arguments
+   * @return array
+   */
+  protected function parseArguments() {
+    $stream = $this->parser->getStream();
+
+    $data = NULL;
+    $only = FALSE;
+
+    if ($stream->test(Twig_Token::BLOCK_END_TYPE)) {
+      $stream->expect(Twig_Token::BLOCK_END_TYPE);
+      return [$data, $only];
     }
 
-    /**
-     * {@inheritdoc}
-     */
-    public function parse(Twig_Token $token)
-    {
-        $component = $this->parser->getExpressionParser()->parseExpression();
-        list($data, $only) = $this->parseArguments();
-
-        return new ComponentNode($component, $data, $only, $token->getLine(), $this->getTag());
+    if ($stream->test(Twig_Token::NAME_TYPE, 'only')) {
+      $only = TRUE;
+      $stream->next();
+      $stream->expect(Twig_Token::BLOCK_END_TYPE);
+      return [$data, $only];
     }
 
-    /**
-     * Parses Tag Arguments
-     * @return array
-     */
-    protected function parseArguments()
-    {
-        $stream = $this->parser->getStream();
+    $data = $this->parser->getExpressionParser()->parseExpression();
 
-        $data = null;
-<<<<<<< HEAD
-        $only = false;
-
-        if ($stream->test(Twig_Token::BLOCK_END_TYPE)) {
-            $stream->expect(Twig_Token::BLOCK_END_TYPE);
-            return [ $data, $only ];
-        }
-
-        if ($stream->test(Twig_Token::NAME_TYPE, 'only')) {
-            $only = true;
-            $stream->next();
-            $stream->expect(Twig_Token::BLOCK_END_TYPE);
-            return [ $data, $only ];
-        }
-
-        $data = $this->parser->getExpressionParser()->parseExpression();
-
-        if ($stream->test(Twig_Token::NAME_TYPE, 'only')) {
-            $only = true;
-            $stream->next();
-        }
-
-        $stream->expect(Twig_Token::BLOCK_END_TYPE);
-=======
-        if ($stream->nextIf(Twig_Token::NAME_TYPE, 'with')) {
-            $data = $this->parser->getExpressionParser()->parseExpression();
-        }
-
-        $only = false;
-        if ($stream->nextIf(Twig_Token::NAME_TYPE, 'only')) {
-            $only = true;
-        }
-
-        $stream->expect(Twig_Token::BLOCK_END_TYPE);
-
->>>>>>> 78298b060d695c124e89fd7ea92a87e9f6326bd4
-        return [ $data, $only ];
+    if ($stream->test(Twig_Token::NAME_TYPE, 'only')) {
+      $only = TRUE;
+      $stream->next();
     }
 
-    /**
-     * {@inheritdoc}
-     */
-    public function getTag()
-    {
-        return 'component';
-    }
+    $stream->expect(Twig_Token::BLOCK_END_TYPE);
+    return [$data, $only];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getTag() {
+    return 'component';
+  }
 }
