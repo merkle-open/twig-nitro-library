cfa022cfa707e7e62219d46f5c4687432fee60f0
Robert Vogt <robert.vogt@namics.com>
2016-04-22 20:50:31 +0200

    Add basic TokenParserTest

--- test/Twig/TokenParser/ComponentTokenParserTest.php
+++ test/Twig/TokenParser/ComponentTokenParserTest.php
@@ -1,13 +1,28 @@
 <?php
 
 namespace Deniaz\Test\Terrific\Twig\TokenParser;
+use Deniaz\Terrific\Twig\TokenParser\ComponentTokenParser;
 
 /**
- * Class ComponentTokenParser
- * @package Deniaz\Test\Terrific\Twig\TokenParser
- * @TODO
+ * Class ComponentTokenParser.
+ *
+ * @TODO Maybe implement test for the Parser.
  */
-class ComponentTokenParserTest
+class ComponentTokenParserTest extends \PHPUnit_Framework_TestCase
 {
+    public function testGetTag()
+    {
+        $parser = new ComponentTokenParser();
+        $this->assertEquals('component', $parser->getTag());
+    }
 
+//    public function testParse()
+//    {
+//        $tokenParser = new ComponentTokenParser();
+//        $parser = $this->getMockBuilder('Twig_Parser')->getMock();
+//        $token = $this->getMockBuilder('Twig_Token')->getMock();
+//
+//        $tokenParser->setParser($parser);
+//        $this->assertInstanceOf(ComponentNode::class, $tokenParser->parse($token));
+//    }
 }
